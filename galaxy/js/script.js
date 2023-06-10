7a14debdd242a8995ad2f2d32f695599628f5027
DkFighT <116903571+DkFighT@users.noreply.github.com>
2023-06-10 23:30:27 +0300

    Add files via upload

--- galaxy/js/script.js
+++ galaxy/js/script.js
@@ -9,10 +9,10 @@ field.addEventListener('mouseup', e => {
     let x = e.clientX;
     let y = e.clientY;
 
-    if (galaxys.length + 1 > 10){
+    if (galaxys.length + 1 > 10) {
         let del_stars = document.querySelectorAll('.star');
         galaxys.splice(0, 10);
-        for (let i = 0; i < del_stars.length; i++){
+        for (let i = 0; i < del_stars.length; i++) {
             del_stars[i].remove();
         }
     }
@@ -22,10 +22,10 @@ field.addEventListener('mouseup', e => {
     galaxys.push(galaxy);
 })
 
-class Planet{
+class Planet {
     #planet;
     #target;
-    constructor(name, size, type, target, num_ring, speed){
+    constructor(name, size, type, target, num_ring, speed) {
         this.name = name;
         this.size = size;
         this.type = type;
@@ -45,7 +45,7 @@ class Planet{
         let radius = (this.num_ring * this.num_ring * 15 + 24) / 2 + this.#target.size / 2;
         let rotate = 0;
         let alpha = this.speed * Math.PI / 180;
-        setInterval(()=>{
+        setInterval(() => {
             rotate += alpha;
             this.#planet.style.top = `${(this.#target.size / 2) + radius * Math.cos(rotate)}px`;
             this.#planet.style.left = `${(this.#target.size / 2) + radius * Math.sin(rotate)}px`;
@@ -54,8 +54,8 @@ class Planet{
     }
 }
 
-class Star extends Planet{
-    constructor(name, size, type, num_rings, x, y){
+class Star extends Planet {
+    constructor(name, size, type, num_rings, x, y) {
         super(name, size, type);
         this.num_rings = num_rings;
         this.x = x;
@@ -70,39 +70,93 @@ class Star extends Planet{
         star.style.top = `${this.y}px`;
         star.style.left = `${this.x}px`;
 
-        for (let i = 1; i <= this.num_rings; i++){
+        for (let i = 1; i <= this.num_rings; i++) {
             star.insertAdjacentHTML('beforeend', `<div id="ring_${i}" class="ring" style="width: ${i * i * 15 + this.size + 20}px; height: ${i * i * 15 + this.size + 20}px; top: 50%; left: 50%;"></div>`);
         }
     }
 }
 
-class Stars{
+class Stars {
     #place;
-    constructor(count, place){
+    constructor(count, place) {
         this.count = count;
         this.#place = place;
     }
     create = () => {
-        for (let i = 0; i < this.count; i++){
+        for (let i = 0; i < this.count; i++) {
             let size = getRandom(1, 3);
             this.#place.insertAdjacentHTML('afterbegin', `<div class="stars" id="star_${i}" style="height: ${size}px; width: ${size}px; top: ${getRandom(0, win_height)}px; left: ${getRandom(0, win_width)}px;"></div>`);
         }
     }
     setAnimation = () => {
-        setInterval(()=>{
+        setInterval(() => {
             let rnd = getRandom(0, 2);
-            if (rnd == 1){
+            let comets = getRandom(0, 100)
+            if (rnd == 1) {
                 document.getElementById('star_' + getRandom(0, 500)).style.opacity = '.3';
             }
-            else{
+            else {
                 document.getElementById('star_' + getRandom(0, 500)).style.opacity = '1';
+            }
+            if (comets == 0) {
+                let comet = new Cometa(getRandom(0, 10000), getRandom(2, 7), parseFloat(`0.${getRandom(0, 10)}`, 10));
+                comet.create_cometa();
+                comet.move();
             }
         }, 1);
     }
 }
 
-class Galaxy{
-    constructor(quantity_planets, x, y){
+class SingletonStars extends Stars {
+    constructor(count, place) {
+        if (SingletonStars._instance) {
+            return SingletonStars._instance;
+        }
+        super(count, place);
+        SingletonStars._instance = this;
+    }
+}
+
+class Cometa {
+    #cometa;
+    constructor(name, size, speed, lengthTail) {
+        this.name = name;
+        this.size = size;
+        this.speed = speed;
+        this.lengthTail = lengthTail;
+        field.insertAdjacentHTML(`beforeend`, `<div id="${this.name}" class="cometa" style="width:${this.size}px; height:${this.size}px;"></div>`);
+        this.#cometa = document.getElementById(this.name);
+        this.start_coords = [getRandom(0, win_width), getRandom(0, win_height)];
+        this.end_coords = [getRandom(0, win_width), getRandom(0, win_height)];
+    }
+    create_cometa = () => {
+        this.#cometa.style.top = `${this.start_coords[1]}px`;
+        this.#cometa.style.left = `${this.start_coords[0]}px`;
+    }
+    move = () => {
+        let dx = this.end_coords[0] - this.start_coords[0];
+        let dy = this.end_coords[1] - this.start_coords[1];
+        let dist = Math.sqrt(dx * dx + dy * dy);
+        let total_time = dist / this.speed;
+        let start_time = new Date().getTime();
+        let cback = setInterval(() => {
+            let now = new Date().getTime();
+            if (now >= start_time + total_time) {
+                this.#cometa.style.left = this.end_coords[0] + "px";
+                this.#cometa.style.top = this.end_coords[1] + "px";
+                clearInterval(cback);
+                this.#cometa.remove();
+            } else {
+                let t = (now - start_time) / total_time;
+                this.#cometa.style.left = (this.start_coords[0] + t * dx) + "px";
+                this.#cometa.style.top = (this.start_coords[1] + t * dy) + "px";
+            }
+        }, 10);
+    }
+}
+
+class Galaxy {
+    constructor(quantity_planets, x, y) {
         this.quantity_planets = quantity_planets;
         this.x = x;
         this.y = y;
@@ -110,7 +164,7 @@ class Galaxy{
     create_galaxy = () => {
         let star = new Star(`Star ${getRandom(1, 10000)}`, getRandom(20, 150), 'star1', this.quantity_planets, this.x, this.y);
         star.set_params();
-        for (let i = 0; i < this.quantity_planets; i++){
+        for (let i = 0; i < this.quantity_planets; i++) {
             let new_planet = new Planet(`Planet ${getRandom(1, 10000)}`, getRandom(10, 41), `type${getRandom(1, 6)}`, star, i + 1, `0.${getRandom(1, 5)}`);
             new_planet.set_params();
             new_planet.move();
@@ -122,6 +176,6 @@ const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-let stars = new Stars(500, field);
+let stars = new SingletonStars(500, field);
 stars.create();
 stars.setAnimation();
